fa2026e939fde3afd32bb5888f343bab0b85f4e7
Carisa Antariksa <antariksacp@gmail.com>
2019-02-04 18:57:29 -0500

    Galaxy bg
    Added in setup

--- sketch1.js
+++ sketch1.js
@@ -7,27 +7,26 @@
  *
  */
 
-var bitsToAsk = 8;  //can be 8 or 16
-var howManyToAsk = 6;
+// var bitsToAsk = 8;  //can be 8 or 16
+// var howManyToAsk = 6;
 
-function setup()
-{
+function setup() {
     createCanvas(windowWidth, windowHeight);
-    background('#00003A');
-    noStroke();
+    background('#00003a');
 
-    for (var i = 0; i < 50; i++) {
-
+    for (var i = 0; i < 100; i++) {
+      var x = random(width);
+      var y = random(height);
+      var r = 2;
+      fill('#ffffff');
+      noStroke();
+      ellipse(x, y, r*2, r*2);
     }
-
+}
     //noStroke();
     //fill(255);  //read the color values from the message
     //textSize(20);
     //text("Click to get "+howManyToAsk+" quantum random Numbers", 5, height/2);
-
-
-}
-
 
 function draw()
 {
@@ -35,31 +34,29 @@ function draw()
 }
 
 
-function mousePressed()
-{
-var askurl = "https://qrng.anu.edu.au/API/jsonI.php?length="+howManyToAsk+"&type=uint"+bitsToAsk;
-
-loadJSON(askurl,whatHappened);   //more details on this function here: https://p5js.org/reference/#/p5/loadJSON
-
-}
-
-
-function whatHappened(returnData)
-{
-console.log(returnData);  //look in the console to see the structure of the returned message
-
-
-background(0,200,255);		//change the background to light blue
-
-	//this loop goes through the array of values returned and draws a circle for each
-	//the amount of circles is determined by how many randoms you ask the API to return
-	for(var i=0;i<returnData.data.length;i++)
-	{
-		noFill();
-		stroke(255);
-		ellipse(width/2,height/2,returnData.data[i],returnData.data[i]);
-
-	}
-
-
-}
+// function mousePressed()
+// {
+// var askurl = "https://qrng.anu.edu.au/API/jsonI.php?length="+howManyToAsk+"&type=uint"+bitsToAsk;
+//
+// loadJSON(askurl,whatHappened);   //more details on this function here: https://p5js.org/reference/#/p5/loadJSON
+//
+// }
+//
+//
+// function whatHappened(returnData)
+// {
+// console.log(returnData);  //look in the console to see the structure of the returned message
+//
+//
+// background(0,200,255);		//change the background to light blue
+//
+// 	//this loop goes through the array of values returned and draws a circle for each
+// 	//the amount of circles is determined by how many randoms you ask the API to return
+// 	for(var i=0;i<returnData.data.length;i++)
+// 	{
+// 		noFill();
+// 		stroke(255);
+// 		ellipse(width/2,height/2,returnData.data[i],returnData.data[i]);
+//
+// 	}
+//
